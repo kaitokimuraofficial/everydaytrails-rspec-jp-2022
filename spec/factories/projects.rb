1557ac34c987d0b41f4b1640f26427e8b6e4e834
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2023-12-31 16:52:58 +0900

    finish chapter 4

---
+++ spec/factories/projects.rb
@@ -0,0 +1,23 @@
+FactoryBot.define do 
+  factory :project do
+    sequence(:name) { |n| "Test Project #{n}" } 
+    description { "Ssample project for testing purposes" } 
+    due_on { 1.week.from_now }
+    association :owner
+    
+    # 昨日が締め切りのプロジェクト
+    factory :project_due_yesterday do
+      due_on { 1.day.ago }
+    end
+    
+    # 今日が締め切りのプロジェクト
+    factory :project_due_today do
+      due_on { Date.current.in_time_zone }
+    end
+    
+    # 明日が締め切りのプロジェクト
+    factory :project_due_tomorrow, class: Project do
+      due_on { 1.day.from_now }
+    end
+  end
+end
